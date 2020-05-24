1c27b278053c9dbf55ac664ad8df582d97114acb
lemarco <igor.hoienko@refinitiv.com>
2020-05-24 20:18:49 +0300

    fixing types for index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import http from 'http';
 const PORT = 3333;
-function listener(request, response) {}
+function listener(req: http.IncomingMessage, res: http.ServerResponse) {}
 
 const server: http.Server = http.createServer(listener);
 server.listen(PORT, () => {
